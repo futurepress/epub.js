a69b82ba852401c21bfabc61e1c440ee4c46faf4
RawKStar77 <RawKStar77@aol.com>
2013-06-05 13:05:47 -0700

    Last commit before Fred's reorginization.

--- grunt.js
+++ grunt.js
@@ -8,16 +8,16 @@ module.exports = function(grunt) {
 		  '<%= grunt.template.today("yyyy-mm-dd") %> */'
 	  },
 	  min: {
-		'dist/render.min.js': ['<banner>', 'fpjs/render/*.js'],
-		//'dist/workers/loader_filesystem.js': ['<banner>', 'fpjs/render/workers/loader_filesystem.js'],
-		'dist/reader.min.js': ['<banner>', 'fpjs/reader/*.js'],
-		'dist/hooks/hooks.min.js': ['<banner>', 'fpjs/hooks/*.js'],
-		'dist/libs/zip.js': ['fpjs/libs/zip.js'],
-		'dist/libs/deflate.js': ['fpjs/libs/deflate.js'],
-		'dist/libs/inflate.js': ['fpjs/libs/inflate.js'],
-		'dist/libs/mime-types.js': ['fpjs/libs/mime-types.js'],
-		'dist/libs/fileStorage.min.js': ['fpjs/libs/fileStorage.min.js'],
-		'dist/libs/loader_filesystem.js': ['fpjs/libs/loader_filesystem.js']
+		'dist/render.min.js': ['<banner>', 'epubjs/render/*.js'],
+		//'dist/workers/loader_filesystem.js': ['<banner>', 'epubjs/render/workers/loader_filesystem.js'],
+		'dist/reader.min.js': ['<banner>', 'epubjs/reader/*.js'],
+		'dist/hooks/hooks.min.js': ['<banner>', 'epubjs/hooks/*.js'],
+		'dist/libs/zip.js': ['epubjs/libs/zip.js'],
+		'dist/libs/deflate.js': ['epubjs/libs/deflate.js'],
+		'dist/libs/inflate.js': ['epubjs/libs/inflate.js'],
+		'dist/libs/mime-types.js': ['epubjs/libs/mime-types.js'],
+		'dist/libs/fileStorage.min.js': ['epubjs/libs/fileStorage.min.js'],
+		'dist/libs/loader_filesystem.js': ['epubjs/libs/loader_filesystem.js']
 	  }
 	});
 	
