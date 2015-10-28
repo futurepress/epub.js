e1537016f1930907c0931465beb543443c177200
Jake Hartnell <Jake.Hartnell@gmail.com>
2015-10-28 13:03:04 -0700

    Update hypothesis.js

--- hooks/extensions/hypothesis.js
+++ hooks/extensions/hypothesis.js
@@ -10,9 +10,9 @@ EPUBJS.Hooks.register("beforeChapterDisplay").hypothesis = function(callback, re
 	EPUBJS.core.addScript("//hypothes.is/embed.js", function() {
     renderer.render.window.hypothesisInstall();
 		// Must add after base css
-		EPUBJS.core.addCss("/reader/css/annotations.css", function() {
-			callback();
-		}, renderer.doc.head);
+		// EPUBJS.core.addCss("/reader/css/annotations.css", function() {
+		// 	callback();
+		// }, renderer.doc.head);
 	}, renderer.doc.head);
 
 };
