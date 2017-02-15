ab4638b10aa2dfbef9d95cd42d893a46708a6cb7
iMefisto <imefisto@gmail.com>
2017-02-15 08:03:44 -0300

    fix css rule

--- src/rendition.js
+++ src/rendition.js
@@ -598,8 +598,8 @@ class Rendition {
 
 		contents.addStylesheetRules([
 			["img",
-				["max-width", (this._layout.columnWidth) + "px; !important"],
-				["max-height", (this._layout.height) + "px; !important"],
+				["max-width", (this._layout.columnWidth) + "px !important"],
+				["max-height", (this._layout.height) + "px !important"],
 				["object-fit", "contain"],
 				["page-break-inside", "avoid"]
 			]
