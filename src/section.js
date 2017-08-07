8bfed1593cfe8ec2f0b5e6896efaf85fac9549fc
Fred Chasen <fchasen@gmail.com>
2017-08-07 16:41:12 -0700

    Fixed spaces in Section

--- src/section.js
+++ src/section.js
@@ -54,7 +54,7 @@ class Section {
 					// var directory = new Url(this.url).directory;
 
 					this.document = xml;
- 					this.contents = xml.documentElement;
+					this.contents = xml.documentElement;
 
 					return this.hooks.content.trigger(this.document, this);
 				}.bind(this))
@@ -96,9 +96,9 @@ class Section {
 					Serializer = require("xmldom").XMLSerializer;
 				} else {
 					Serializer = XMLSerializer;
-				}				
- 				var serializer = new Serializer();
- 				this.output = serializer.serializeToString(contents);
+				}
+				var serializer = new Serializer();
+				this.output = serializer.serializeToString(contents);
 				return this.output;
 			}.bind(this)).
 			then(function(){
@@ -114,64 +114,64 @@ class Section {
 		return rendered;
 	}
 
-        /**
-         * Find a string in a section
-         * @param  {string} _query The query string to find
-         * @return {object[]} A list of matches, with form {cfi, excerpt}
-         */
-        find(_query){
-                var section = this;
-                var matches = [];
-                var query = _query.toLowerCase();
-                var find = function(node){
-                        var text = node.textContent.toLowerCase();
-                        var range = section.document.createRange();
-                        var cfi;
-                        var pos;
-                        var last = -1;
-                        var excerpt;
-                        var limit = 150;
+	/**
+	 * Find a string in a section
+	 * @param  {string} _query The query string to find
+	 * @return {object[]} A list of matches, with form {cfi, excerpt}
+	 */
+	find(_query){
+		var section = this;
+		var matches = [];
+		var query = _query.toLowerCase();
+		var find = function(node){
+			var text = node.textContent.toLowerCase();
+			var range = section.document.createRange();
+			var cfi;
+			var pos;
+			var last = -1;
+			var excerpt;
+			var limit = 150;
 
-                        while (pos != -1) {
-                                // Search for the query
-                                pos = text.indexOf(query, last + 1);
+			while (pos != -1) {
+				// Search for the query
+				pos = text.indexOf(query, last + 1);
 
-                                if (pos != -1) {
-                                        // We found it! Generate a CFI
-                                        range = section.document.createRange();
-                                        range.setStart(node, pos);
-                                        range.setEnd(node, pos + query.length);
+				if (pos != -1) {
+					// We found it! Generate a CFI
+					range = section.document.createRange();
+					range.setStart(node, pos);
+					range.setEnd(node, pos + query.length);
 
-                                        cfi = section.cfiFromRange(range);
+					cfi = section.cfiFromRange(range);
 
-                                        // Generate the excerpt
-                                        if (node.textContent.length < limit) {
-                                                excerpt = node.textContent;
-                                        }
-                                        else {
-                                                excerpt = node.textContent.substring(pos - limit/2, pos + limit/2);
-                                                excerpt = "..." + excerpt + "...";
-                                        }
+					// Generate the excerpt
+					if (node.textContent.length < limit) {
+						excerpt = node.textContent;
+					}
+					else {
+						excerpt = node.textContent.substring(pos - limit/2, pos + limit/2);
+						excerpt = "..." + excerpt + "...";
+					}
 
-                                        // Add the CFI to the matches list
-                                        matches.push({
-                                                cfi: cfi,
-                                                excerpt: excerpt
-                                        });
-                                }
+					// Add the CFI to the matches list
+					matches.push({
+						cfi: cfi,
+						excerpt: excerpt
+					});
+				}
 
-                                last = pos;
-                        }
-                };
+				last = pos;
+			}
+		};
 
-                sprint(section.document, function(node) {
-                        find(node);
-                });
+		sprint(section.document, function(node) {
+			find(node);
+		});
 
-                return matches;
-        };
+		return matches;
+	};
 
-       /**
+	/**
 	* Reconciles the current chapters layout properies with
 	* the global layout properities.
 	* @param {object} global  The globa layout settings object, chapter properties string
