ba09ee28b8e6ed281da34eb394a15ced40559fc5
Fred Chasen <fchasen@gmail.com>
2017-11-03 16:03:05 -0700

    Fix selected event

--- src/contents.js
+++ src/contents.js
@@ -728,7 +728,7 @@ class Contents {
 			if(!range.collapsed) {
 				// cfirange = this.section.cfiFromRange(range);
 				cfirange = new EpubCFI(range, this.cfiBase).toString();
-				this.emit(EVENTS.CONTENTS.ELECTED, cfirange);
+				this.emit(EVENTS.CONTENTS.SELECTED, cfirange);
 				this.emit(EVENTS.CONTENTS.SELECTED_RANGE, range);
 			}
 		}
