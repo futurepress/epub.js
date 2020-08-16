6642c435ca336b5e4948fdbfc48f8c3c5d008fee
Massimiliano Mirra <bard@hyperstruct.net>
2020-08-16 10:40:10 +0100

    update Epub() type signature to reflect implementation

--- types/epub.d.ts
+++ types/epub.d.ts
@@ -2,5 +2,5 @@ import Book, { BookOptions } from "./book";
 
 export default Epub;
 
-declare function Epub(url: string, options?: BookOptions) : Book;
+declare function Epub(urlOrData: string | ArrayBuffer, options?: BookOptions) : Book;
 declare function Epub(options?: BookOptions) : Book;
